7e67f66c379a0d4575c883a1ffcf91b94a373728
EdyAtthoillah123 <112244332+EdyAtthoillah123@users.noreply.github.com>
2024-03-05 03:59:59 +0700

    Feat : FrontEnd Service Software Page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('service_software');
 });
 
 Route::get('/dashboard', function () {
